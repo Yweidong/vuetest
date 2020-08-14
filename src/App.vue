f7bcc942e89a19e5b512a1ef5e2094429bf62032
wei_tung <1371690483@qq.com>
2020-08-14 17:17:10 +0800

    second

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <home>
-     <router-link to="/home">Home</router-link>
-        <router-link to="/test">Test</router-link>
-      <router-view></router-view>
-    </home>
+    <router-view />
   </div>
 </template>
 
 <script>
-
-
-
 export default {
-  name: 'App',
-  components: {
-
-
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
