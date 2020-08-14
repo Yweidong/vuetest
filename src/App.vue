9db2c3ee2f087715f9118c296f718a7e82bc41e6
wei_tung <1371690483@qq.com>
2020-08-14 15:21:22 +0800

    vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <home>
+     <router-link to="/home">Home</router-link>
+        <router-link to="/test">Test</router-link>
+      <router-view></router-view>
+    </home>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+
   }
 }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+
+
+    <p>{{message}}</p>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                message: "home"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
